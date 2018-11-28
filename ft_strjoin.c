4c0bbf2ed1edb7be9d0d89440841e8e25287c434
Jordan AYACHE <jayache@e3r5p15.42.fr>
2018-11-28 10:52:06 +0100

    putchar unicode friendly

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: jayache <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 13:37:49 by jayache           #+#    #+#             */
-/*   Updated: 2018/11/10 16:26:16 by jayache          ###   ########.fr       */
+/*   Updated: 2018/11/28 10:49:01 by jayache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	x = 0;
 	if (!s1 || !s2)
 		return (NULL);
-	if (!ft_tmalloc((void*)&str, (ft_strlen(s1) + ft_strlen(s2) + 1)))
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (NULL);
 	while (s1[a])
 	{
